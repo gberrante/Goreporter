526ef53465a14b81d98bdcf39d7c5f4eee46a762
errante.gianni <errante.gianni@gmail.com>
2018-05-21 22:31:29 +0200

    External command trial

--- reporter.go
+++ reporter.go
@@ -1,12 +1,20 @@
 package main
 
-import "os"
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
 
 	//argomento 0 è il path di questo exe, argomento 1 è quello inserito
 	// i successivi argomenti separati dallo spazio sono ignorati
-	path := os.Args[1]
-	fmt.Println(path)
+	//path := os.Args[1]
+	//fmt.Println(path)
+	out, err := exec.Command("git", "--version").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", out)
 }
