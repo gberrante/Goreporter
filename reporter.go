1cd3298ed276409906d6034a5112b9899694fbdd
errante.gianni <errante.gianni@gmail.com>
2018-05-22 19:39:09 +0200

    Dos command works

--- reporter.go
+++ reporter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
@@ -10,9 +11,11 @@ func main() {
 
 	//argomento 0 è il path di questo exe, argomento 1 è quello inserito
 	// i successivi argomenti separati dallo spazio sono ignorati
-	//path := os.Args[1]
-	//fmt.Println(path)
-	out, err := exec.Command("git", "--version").Output()
+	path := os.Args[1]
+	fmt.Println(path)
+	command := "--git-dir=" + path
+	fmt.Println(command)
+	out, err := exec.Command("git", command, "log", "--pretty=%aN;%aI").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
