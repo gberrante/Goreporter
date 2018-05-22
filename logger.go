b46ef566022d6a4e2ce1a2c3f240e53b0d7175aa
errante.gianni <errante.gianni@gmail.com>
2018-05-22 21:32:54 +0200

    Out parsing

--- logger.go
+++ logger.go
@@ -1,9 +1,22 @@
 package main
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+)
 
 func logger(path string) string {
 	command := "--git-dir=" + path
-	out, _ := exec.Command("git", command, "log", "--pretty=%aN;%aI").Output()
-	return string(out[:])
+	out, _ := exec.Command("git", command, "log", "--pretty=%an;%ai").Output()
+	text := string(out[:])
+	var expr = regexp.MustCompile(`\n`)
+	lines := expr.FindStringSubmatch(text)
+	imax := len(lines)
+	for i := 1; i < imax; i++ {
+		fmt.Println(lines[i])
+	}
+	//var expr = regexp.MustCompile(`^(.*);(\d+)-(\d+)-(\d+)`)
+	//token := expr.FindStringSubmatch(string(out[:]))
+	return "fine"
 }
--- reporter.go
+++ reporter.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
@@ -10,5 +9,5 @@ func main() {
 	//argomento 0 è il path di questo exe, argomento 1 è quello inserito
 	// i successivi argomenti separati dallo spazio sono ignorati
 	path := os.Args[1]
-	fmt.Printf("%s\n", logger(path))
+	logger(path)
 }
