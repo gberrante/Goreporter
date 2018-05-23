5983b2d1887ec80fc457f5ec1764da4a3af08d35
errante.gianni <errante.gianni@gmail.com>
2018-05-23 19:29:51 +0200

    Update commit log function

--- logger.go
+++ logger.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"regexp"
+	"strconv"
 )
 
-func logger(path string) string {
+//commit data structure
+type Commits struct {
+	name  string
+	year  int
+	month int
+	day   int
+}
+
+func logger(path string) []Commits {
 	command := "--git-dir=" + path
 	out, _ := exec.Command("git", command, "log", "--pretty=%an;%ai").Output()
 	text := string(out[:])
-	var expr = regexp.MustCompile(`\n`)
+	var expr = regexp.MustCompile(`(.*)\n`)
 	lines := expr.FindStringSubmatch(text)
 	imax := len(lines)
-	for i := 1; i < imax; i++ {
-		fmt.Println(lines[i])
+	list := []Commits{}
+
+	for i := 0; i < imax; i++ {
+		token := regexp.MustCompile(`^(.+);(\d+)-(\d+)-(\d+)`).FindStringSubmatch(lines[i])
+		temp := Commits{}
+		temp.name = token[1]
+		temp.year, _ = strconv.Atoi(token[2])
+		temp.month, _ = strconv.Atoi(token[3])
+		temp.day, _ = strconv.Atoi(token[4])
+		list = append(list, temp)
 	}
-	//var expr = regexp.MustCompile(`^(.*);(\d+)-(\d+)-(\d+)`)
-	//token := expr.FindStringSubmatch(string(out[:]))
-	return "fine"
+	return list
 }
--- reporter.go
+++ reporter.go
@@ -5,7 +5,6 @@ import (
 )
 
 func main() {
-
 	//argomento 0 è il path di questo exe, argomento 1 è quello inserito
 	// i successivi argomenti separati dallo spazio sono ignorati
 	path := os.Args[1]
