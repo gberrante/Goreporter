370dfe19751ba53244359597299c200799fa6aeb
errante.gianni <errante.gianni@gmail.com>
2018-05-26 08:06:39 +0200

    Parsing Update

--- logger.go
+++ logger.go
@@ -1,36 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
-	"regexp"
-	"strconv"
+	"strings"
 )
 
 //commit data structure
 type Commits struct {
-	name  string
-	year  int
-	month int
-	day   int
+	name   string
+	year   int
+	month  int
+	day    int
+	add    int
+	delete int
 }
 
-func logger(path string) []Commits {
+func logger(path string) int {
 	command := "--git-dir=" + path
-	out, _ := exec.Command("git", command, "log", "--pretty=%an;%ai").Output()
+	out, _ := exec.Command("git", command, "log", "--pretty=#BEGIN%an;%ai", "--numstat").Output()
 	text := string(out[:])
-	var expr = regexp.MustCompile(`(.*)\n`)
-	lines := expr.FindStringSubmatch(text)
-	imax := len(lines)
-	list := []Commits{}
-
+	blocks := strings.SplitAfter(text, "#BEGIN")
+	imax := len(blocks)
+	blocks = blocks[1:]
 	for i := 0; i < imax; i++ {
-		token := regexp.MustCompile(`^(.+);(\d+)-(\d+)-(\d+)`).FindStringSubmatch(lines[i])
+		lines := strings.SplitAfter(blocks[i], "\n")
+		lines = lines[:len(lines)-1]
+		lines = append(lines[:1], lines[2:]...)
+		fmt.Println(lines[0])
+		/*token := regexp.MustCompile(`^#BEGIN(.+);(\d+)-(\d+)-(\d+)`).FindStringSubmatch(lines[i])
 		temp := Commits{}
 		temp.name = token[1]
 		temp.year, _ = strconv.Atoi(token[2])
 		temp.month, _ = strconv.Atoi(token[3])
 		temp.day, _ = strconv.Atoi(token[4])
-		list = append(list, temp)
+		list = append(list, temp)*/
 	}
-	return list
+	return 0
 }
