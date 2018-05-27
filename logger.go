4dc22ae61b3d6406977507a00d33ee0b9dc38f9d
errante.gianni <errante.gianni@gmail.com>
2018-05-27 10:43:20 +0200

    Parsing Update

--- logger.go
+++ logger.go
@@ -1,40 +1,64 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 //commit data structure
 type Commits struct {
-	name   string
-	year   int
-	month  int
-	day    int
-	add    int
-	delete int
+	name     string
+	year     int
+	month    int
+	day      int
+	add      int
+	delete   int
+	filesMod int
 }
 
-func logger(path string) int {
+func logger(path string) []Commits {
 	command := "--git-dir=" + path
 	out, _ := exec.Command("git", command, "log", "--pretty=#BEGIN%an;%ai", "--numstat").Output()
 	text := string(out[:])
 	blocks := strings.SplitAfter(text, "#BEGIN")
 	imax := len(blocks)
 	blocks = blocks[1:]
-	for i := 0; i < imax; i++ {
+	list := []Commits{}
+	for i := 0; i < imax-1; i++ {
 		lines := strings.SplitAfter(blocks[i], "\n")
 		lines = lines[:len(lines)-1]
 		lines = append(lines[:1], lines[2:]...)
-		fmt.Println(lines[0])
-		/*token := regexp.MustCompile(`^#BEGIN(.+);(\d+)-(\d+)-(\d+)`).FindStringSubmatch(lines[i])
+		//prima linea ha il titolo del commit
+		token := regexp.MustCompile(`^(.+);(\d+)-(\d+)-(\d+)`).FindStringSubmatch(lines[0])
 		temp := Commits{}
 		temp.name = token[1]
 		temp.year, _ = strconv.Atoi(token[2])
 		temp.month, _ = strconv.Atoi(token[3])
 		temp.day, _ = strconv.Atoi(token[4])
-		list = append(list, temp)*/
+
+		//dalla seconda linea
+		added := 0
+		removed := 0
+		modified := 0
+		for j := 1; j < len(lines); j++ {
+			expr, _ := regexp.Compile(`(\d+)\s(\d+)\s.*`)
+			if expr.MatchString(lines[j]) {
+				token := expr.FindStringSubmatch(lines[j])
+				adj, _ := strconv.Atoi(token[1])
+				rmj, _ := strconv.Atoi(token[2])
+				added += adj
+				removed += rmj
+				modified++
+			} else {
+				continue
+			}
+		}
+		temp.filesMod = modified
+		temp.add = added
+		temp.delete = removed
+		list = append(list, temp)
 	}
-	return 0
+	return list
 }
--- reporter.go
+++ reporter.go
@@ -8,5 +8,5 @@ func main() {
 	//argomento 0 è il path di questo exe, argomento 1 è quello inserito
 	// i successivi argomenti separati dallo spazio sono ignorati
 	path := os.Args[1]
-	logger(path)
+	/*Stats :=*/ logger(path)
 }
