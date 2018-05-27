5c1a22715a64b34132cdffcac648418a1e6eb1b0
errante.gianni <errante.gianni@gmail.com>
2018-05-27 21:19:34 +0200

    Hash table statistics fixed

--- generator.go
+++ generator.go
@@ -16,23 +16,23 @@ func Generator(data []Commits) {
 	DateMap := make(map[string]Counter)
 	WeekMap := make(map[string]Counter)
 
+	//Scan Data and remove duplicates
 	for i := range data {
-		if NameMap[data[i].name].exist == false {
-			value := NameMap[data[i].name].numCom
+		if !NameMap[data[i].name].exist {
 			NameMap[data[i].name] = Counter{exist: true}
-			NameMap[data[i].name] = Counter{numCom: value + 1}
 		}
-		if DateMap[data[i].date].exist == false {
-			value := DateMap[data[i].date].numCom
+		if !DateMap[data[i].date].exist {
 			DateMap[data[i].date] = Counter{exist: true}
-			DateMap[data[i].date] = Counter{numCom: value + 1}
 		}
-		if WeekMap[data[i].week].exist == false {
-			value := WeekMap[data[i].week].numCom
+		if !WeekMap[data[i].week].exist {
 			WeekMap[data[i].week] = Counter{exist: true}
-			WeekMap[data[i].week] = Counter{numCom: value + 1}
 		}
 	}
-
+	//Count data occurence for each item in Map
+	for i := range data {
+		NameMap[data[i].name] = Counter{exist: true, numCom: NameMap[data[i].name].numCom + 1}
+		DateMap[data[i].date] = Counter{exist: true, numCom: DateMap[data[i].date].numCom + 1}
+		WeekMap[data[i].week] = Counter{exist: true, numCom: WeekMap[data[i].week].numCom + 1}
+	}
 	fmt.Println("debug")
 }
