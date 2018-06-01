d09cacf0288227055449c89661d114e0d99b5f85
errante.gianni <errante.gianni@gmail.com>
2018-06-01 09:16:39 +0200

    Json Generator up

--- generator.go
+++ generator.go
@@ -1,50 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 )
 
-type Counter struct {
-	exist  bool
-	numCom int
-}
+var Authors [][]string
+var Days [][]string
+var Weeks [][]string
 
 // Generator of committer map, and other relative lists
 func Generator(data []Commits) {
 	//Committer HASH Table
-	NameMap := make(map[string]Counter)
-	DateMap := make(map[string]Counter)
-	WeekMap := make(map[string]Counter)
+	NameMap := make(map[string]int)
+	DateMap := make(map[string]int)
+	WeekMap := make(map[string]int)
 
 	//Scan Data and remove duplicates
 	for i := range data {
-		if !NameMap[data[i].name].exist {
-			NameMap[data[i].name] = Counter{exist: true}
-		}
-		if !DateMap[data[i].date].exist {
-			DateMap[data[i].date] = Counter{exist: true}
-		}
-		if !WeekMap[data[i].week].exist {
-			WeekMap[data[i].week] = Counter{exist: true}
-		}
+		NameMap[data[i].name]++
+		DateMap[data[i].date]++
+		WeekMap[data[i].week]++
 	}
-	//Count data occurence for each item in Map
-	for i := range data {
-		NameMap[data[i].name] = Counter{exist: true, numCom: NameMap[data[i].name].numCom + 1}
-		DateMap[data[i].date] = Counter{exist: true, numCom: DateMap[data[i].date].numCom + 1}
-		WeekMap[data[i].week] = Counter{exist: true, numCom: WeekMap[data[i].week].numCom + 1}
+	jsonName, _ := json.Marshal(NameMap)
+	jsonDate, _ := json.Marshal(DateMap)
+	jsonWeek, _ := json.Marshal(WeekMap)
+	file, err := os.Create("result.json")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
 	}
-	/*
-		NameK := ExtractKey(NameMap)
-		DateK := ExtractKey(DateMap)
-		WeeK := ExtractKey(WeekMap)*/
-	fmt.Println("ees")
-}
-
-func ExtractKey(table map[string]Counter) []string {
-	var keys []string
-	for key, _ := range table {
-		keys = append(keys, key)
-	}
-	return keys
+	defer file.Close()
+	fmt.Fprintf(file, "%s\n%s\n%s", jsonName, jsonDate, jsonWeek)
 }
